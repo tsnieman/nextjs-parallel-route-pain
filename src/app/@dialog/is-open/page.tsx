7181d88e53432bd171c134f9c97b73c7767665c9
tyler <tyler@ressiosoftware.com>
2023-12-08 12:01:04 -0800

    add example: parallel route + refresh = client router broken
    related reading:
    - https://github.com/vercel/next.js/issues/54173

---
+++ src/app/@dialog/is-open/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+import prisma from '@/db';
+import { revalidatePath } from "next/cache";
+
+export default function Page() {
+	async function createItem() {
+		"use server";
+
+		// create a new blog post
+		await prisma.blogPost.create({
+			data: {
+				content: "item created at " + new Date().toISOString(),
+			},
+		});
+
+		// make sure all the data on the page reloads
+		revalidatePath('/', 'page');
+	}
+
+	return (
+		<dialog open className="border-solid border-2 border-indigo-600 shadow-xl min-w-[500px] min-h-[500px]">
+			<h1>{`i'm a modal`}</h1>
+
+			<br />
+
+			<form action={createItem}>
+				<button type="submit" className="button">CREATE NEW BLOG POST</button>
+			</form>
+
+			<br />
+
+			<Link href="/" className="text-blue-500 underline">
+				LINK BACK TO HOME PAGE
+			</Link>
+		</dialog>
+	);
+}
